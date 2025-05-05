499210fffe295cd28cd5f3c93f20911c44435320
youssefkhaled <43790986+youssefkhaled2019@users.noreply.github.com>
2025-05-05 14:11:22 +0200

    hhhh

--- main2.py
+++ main2.py
@@ -9,6 +9,7 @@ from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 import os,django
 import pyautogui
+import time
 
 # RUNSERVER
 async def runserver():
@@ -19,38 +20,15 @@ async def runserver():
     os.system("py manage.py runserver")
 
 # OPEN BROWSER
-def openproject():
-    # webbrowser.open_new_tab("http://127.0.0.1:8000/")
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    path=os.path.join(BASE_DIR,"Djangotasks" )
-    # print(path)
-    sys.path.append(path)
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Main.settings")
-    django.setup()
-
-
-    app = QApplication(sys.argv)
-    window = QMainWindow()
-    view = QWebEngineView()
-
-    application = get_wsgi_application()
-
-
-    # call_command('runserver',  '127.0.0.1:8000')
-
-    view.load(QUrl('http://localhost:8000/')) #Load the Django URL
-    window.setCentralWidget(view)
-    window.show()
-
-    sys.exit(app.exec_())
-    os.system("py manage.py runserver")
-
+# def openproject():
+#     # webbrowser.open_new_tab("http://127.0.0.1:8000/")
+#     pass
 
 # EXECUTE PROGRAM
 async def main():
-    # task1 = asyncio.create_task(runserver())
-    openproject()
-    # await task1
+    task1 = asyncio.create_task(runserver())
+    # openproject()
+    await task1
 
 
 
